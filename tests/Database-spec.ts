e2ce3bef4e6b75731e1b34e524811e5672c6351b
Ken Snyder <ken@ken.net>
2021-02-10 11:30:33 -0800

    add arrayToObject and new tests

--- tests/Database-spec.ts
+++ tests/Database-spec.ts
@@ -1,3 +1,4 @@
+import { arrayToObject } from "~/arrayToObject";
 import { Database } from "~/Database";
 import { Table } from "~/Table";
 import { mySong, Playlist, Song } from "./data";
@@ -53,5 +54,47 @@ describe("Trouble with iterables => ", () => {
     expect(typeof tables.Song.is).toBe("function");
     expect(tables.Song.is(mySong)).toBe(true);
     expect(tables.Playlist.is(mySong)).toBe(false);
+  });
+});
+
+describe("arrayToObject => ", () => {
+  it("array of relatively simple name/value objects with different types for 'value'", () => {
+    const foo = { name: "foo", value: 123 };
+    const bar = { name: "bar", value: "bar" };
+    const baz = { name: "baz", value: new Date() };
+
+    const arr = [foo, bar, baz];
+    const result = arrayToObject(arr);
+
+    expect(typeof result).toBe("object");
+
+    expect(typeof result.foo).toBe("object");
+    expect(typeof result.foo.value).toBe("number");
+    expect(result.foo.value.toFixed(2)).toBe(foo.value.toFixed(2));
+
+    expect(typeof result.bar).toBe("object");
+    expect(typeof result.bar.value).toBe("string");
+    expect(result.bar.name).toBe("bar");
+    expect(result.bar.value.toUpperCase()).toBe(bar.value.toUpperCase());
+  });
+
+  it("array of objects which carry type info in generics", () => {
+    const song = Table(Song);
+    const playlist = Table(Playlist);
+
+    // here we can see the type inference working
+    expect(song.select("artist")).toBe("this is just a test");
+
+    const arr = [song, playlist];
+    const result = arrayToObject(arr);
+
+    expect(result.Song.name).toBe("Song");
+    // type guards of the given type are preserved
+    // due to `io-ts` run-time def
+    expect(result.Song.is(mySong)).toBe(true);
+    expect(result.Song.is({ id: "not-a-song" })).toBe(false);
+
+    // type inference fails to provide non-io-ts properties
+    expect(result.Song.select("artist")).toBe("this is just a test");
   });
 });
